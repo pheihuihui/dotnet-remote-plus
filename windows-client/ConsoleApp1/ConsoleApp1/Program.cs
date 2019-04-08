93b6896ccb38217a4902c9dc70ed4fdf5e533ef2
Fei <33122873+pheihuihui@users.noreply.github.com>
2019-04-08 18:26:20 +0800

    ...

---
+++ windows-client/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connect("127.0.0.1", "ssss");
+        }
+
+        static void Connect(string server, string message)
+        {
+            try
+            {
+                int port = 13000;
+                TcpClient client = new TcpClient(server, port);
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+                data = new byte[256];
+                string responseData = string.Empty;
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
